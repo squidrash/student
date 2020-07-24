194b075a7730cbff3b4212199c795897764b0139
nikitarasevskij <squidrash777@gmail.com>
2020-07-24 12:49:03 +0300

    intt

--- 1_Карточка_студента_R/Program.cs
+++ 1_Карточка_студента_R/Program.cs
@@ -1,14 +1,65 @@
 ﻿using System;
+using System.Text.Json;
+using Student;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
 
 namespace _1_Карточка_студента_R
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //111
-            ///121212
+            List<StudentDTO> students = new List<StudentDTO>();
+            ///121
+            StudentDTO ivan = new StudentDTO()
+            {
+                FIO = "Иванов Иван Иванович",
+                curriculum = new Сurriculum()
+                {
+                    Faculty = "Химия",
+                    Speciality = "Органика",
+                    Course = 4,
+                    Group = "хим-бо-14-1"
+                },
+                address = new Address()
+                {
+                    City = "Ставрополь",
+                    PostIndex = 35500,
+                    Street = "К. Хетагурова"
+                },
+                contact = new Contacts()
+                {
+                    Phone = 89620000006,
+                    Email = "ivan@mail.ru"
+                }
+
+            };
+            using (FileStream fs = new FileStream("student.json", FileMode.OpenOrCreate))
+            {
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                };
+
+                await JsonSerializer.SerializeAsync<StudentDTO>(fs, ivan, options);
+
+                Console.WriteLine("Сохранено в файл");
+                Console.WriteLine($"{ivan.curriculum.Faculty}, {ivan.curriculum.Speciality}," +
+                    $"{ ivan.curriculum.Course} { ivan.curriculum.Group}");
+            }
+            using (FileStream fs = new FileStream("student.json", FileMode.OpenOrCreate))
+            {
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                };
+                StudentDTO student = await JsonSerializer.DeserializeAsync<StudentDTO>(fs, options);
+                Console.WriteLine($"FIO: {student.FIO}, Faculty: {student.curriculum.Faculty}, Speciality: {student.curriculum.Speciality}," +
+                    $" Course: {student.curriculum.Course}, Gpoup: {student.curriculum.Group}, City: {student.address.City}, PostIndex: {student.address.PostIndex}, " +
+                    $"Street: {student.address.Street}, Phone: {student.contact.Phone}, Email: {student.contact.Email}");
+            }
         }
     }
 }
---
+++ Student/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Student
+{
+    public class StudentDTO
+    {
+        public string FIO { get; set; }
+        public Сurriculum curriculum;
+        public Address address;
+        public Contacts contact;
+    }
+
+    public class Сurriculum
+    {
+        public string Faculty { get; set; }
+        public string Speciality { get; set; }
+        public int Course { get; set; }
+        public string Group { get; set; }
+    }
+    public class Address
+    {
+        public string City { get; set; }
+        public int PostIndex { get; set; }
+        public string Street { get; set; }
+    }
+    public class Contacts
+    {
+        public long Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
