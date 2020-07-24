2f30821135b3bd1cac38dbef5ccb8e891e7799e0
nikitarasevskij <squidrash777@gmail.com>
2020-07-24 12:33:46 +0300

    init

---
+++ 1_Карточка_студента_R/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_Карточка_студента_R
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
